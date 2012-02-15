2eb58265296315318e2ad8aab8bfeff9eb16954e
Sean Brewer <seabre986@gmail.com>
2012-02-15 11:44:38 -0500

    Modified delayed_job recipe slightly.

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -3,22 +3,22 @@
 # Recipe:: default
 #
 
-if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
+if node[:instance_role] == "app_master" || node[:instance_role] == "app" || node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:name] !~ /^(mongodb|redis|memcache)/)
   node[:applications].each do |app_name,data|
-  
+
     # determine the number of workers to run based on instance size
-    if node[:instance_role] == 'solo'
+    if node[:instance_role] == 'solo' || node[:instance_role] == "app_master" || node[:instance_role] == "app"
       worker_count = 1
     else
       case node[:ec2][:instance_type]
       when 'm1.small': worker_count = 2
       when 'c1.medium': worker_count = 4
       when 'c1.xlarge': worker_count = 8
-      else 
+      else
         worker_count = 2
       end
     end
-    
+
     worker_count.times do |count|
       template "/etc/monit.d/delayed_job#{count+1}.#{app_name}.monitrc" do
         source "dj.monitrc.erb"
@@ -34,11 +34,11 @@ if node[:instance_role] == "solo" || (node[:instance_role] == "util" && node[:na
         })
       end
     end
-    
+
     execute "monit reload" do
        action :run
        epic_fail true
     end
-      
+
   end
 end
